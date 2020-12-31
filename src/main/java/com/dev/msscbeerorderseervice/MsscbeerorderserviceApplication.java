f2f748bfc23a49855e3a859a6ca6b7444646f265
devpunumalli <devpunumalli@gmail.com>
2020-12-31 12:15:29 -0500

    Add DB Init scripts,closes #6

--- src/main/java/com/dev/msscbeerorderseervice/MsscbeerorderserviceApplication.java
+++ src/main/java/com/dev/msscbeerorderseervice/MsscbeerorderserviceApplication.java
@@ -2,10 +2,10 @@ package com.dev.msscbeerorderseervice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableJpaRepositories("com.dev.msscbeerorderseervice")
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class MsscbeerorderserviceApplication {
 
     public static void main(String[] args) {
